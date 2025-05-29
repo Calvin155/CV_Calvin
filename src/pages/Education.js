42b289f147b7b6d59a3d28ea42adc3fde7e18214
Calvin155 <calvinlynch181@outlook.com>
2025-05-29 15:05:13 +0100

    small update tag

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -37,7 +37,7 @@ const Education = () => {
               <h4>4th Year <span className="gpa">GPA: 74.81%</span></h4>
               <p><strong>Modules:</strong> Secure Public Cloud Services (69%), Reactive Systems (82%), Enterprise Application Development (72%), Management in Tech Software Engineering 1 (76%), Big Data Mining and Analysis (Elective) (59%)</p>
               <p>***Awaiting Second Semester Results***</p>
-              <p>Modules: 4th Year Modules – Api Design & Development, Machine Learning & Predictive Analysis, Software Management Techniques 2, Software Quality Engineering, Penetration Testing (Elective)</p>
+              <p><strong>Modules:</strong> 4th Year Modules – Api Design & Development, Machine Learning & Predictive Analysis, Software Management Techniques 2, Software Quality Engineering, Penetration Testing (Elective)</p>
             </div>
           </div>
 
